c2297ce892bfd0e780fc0db1a80df8b1ff7b1084
pg2000 <patrick.geschinski@gmail.com>
2021-03-02 14:13:01 +0100

    customize dynect client

--- pkg/dynsoap/client.go
+++ pkg/dynsoap/client.go
@@ -6,19 +6,22 @@ import (
 	"time"
 )
 
-// Returns a Dynect Client
-// If no http.client is given. A default one will be used
+// Returns a Dynect Client with a configured http.Client
 // The default settings for the http.client has a timeout of
 // 10 seconds and reads proxy variables from environment http.ProxyFromEnvironment
-func NewCustomDynectClient(url string, client *http.Client) Dynect {
-	if client == nil {
-		client = &http.Client{
-			Timeout: time.Second * 10,
-			Transport: &http.Transport{
-				Proxy: http.ProxyFromEnvironment,
-			},
-		}
+func NewDynectClient(url string) Dynect {
+	client := &http.Client{
+		Timeout: time.Second * 10,
+		Transport: &http.Transport{
+			Proxy: http.ProxyFromEnvironment,
+		},
 	}
 	soapClient := soap.NewClient(url, soap.WithHTTPClient(client))
+	return NewDynect(soapClient)
+}
+
+// Returns a Dynect Client without http.Client
+func NewCustomDynectClient(url string, client http.Client) Dynect {
+	soapClient := soap.NewClient(url, soap.WithHTTPClient(&client))
 	return NewDynect(soapClient)
 }
